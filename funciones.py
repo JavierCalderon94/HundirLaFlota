7bcdb28c20fc220fed72f8a174f77e07ad6cc0e6
JavierCalderon94 <javiercalderongarcia@gmail.com>
2024-02-23 23:48:40 +0100

    100% Operativo (Faltan clases)

--- funciones.py
+++ funciones.py
@@ -5,56 +5,10 @@ import time
 def crear_tablero(tamaño=(10,10)):
     return np.full(tamaño, "_")
 
-def colocar_barco(barco, tablero):
-    for casilla in barco:
-        tablero[casilla] = "O"
-    return tablero
-
-def colocar_barcos(barcos, tablero):
-    for barco in barcos:
-        tablero = colocar_barco(barco, tablero)
-    return tablero
-
-def disparar(casilla, tablero):
-    repetir = True
-
-    if tablero[casilla] == "_":
-        print("Agua")
-        tablero[casilla] = "A"
-        repetir = False
-
-    elif tablero[casilla] == "O":
-        print("Tocado, vuelves a disparar")
-        tablero[casilla] = "X"
-
-    else:
-        print("Ya has disparado aquí, vuelve a intentarlo")
-
-    return tablero, repetir
-
-def disparo_random(tablero):
-    fila_a = random.randint(0,len(tablero)-1)
-    columna_a = random.randint(0,len(tablero)-1)
-    casilla = (fila_a,columna_a)
-    fallo = True
-    repetir = False
-
-    while fallo:
-        if tablero[casilla] == "_":
-            print("Agua")
-            tablero[casilla] = "A"
-            fallo = False
-
-        elif tablero[casilla] == "O":
-            print("Tocado, la máquina dispara de nuevo")
-            tablero[casilla] = "X"
-            repetir = True
-            fallo = False
-
-        else:
-            continue
-
-    return tablero, repetir
+def ocultar_tablero(tablero):
+    tab_oculto = tablero.copy()
+    tab_oculto[(tablero == "_") | (tablero == "O")] = "░"
+    print("\n",tab_oculto,"\n ------------------------------------------------------------\n")
 
 def crear_barco_random(eslora,tablero):
     barco =[]
@@ -93,43 +47,143 @@ def crear_barco_random(eslora,tablero):
 def colocar_barco(barco, tablero):
     for casilla in barco:
         tablero[casilla] = "O"
-    return tablero
+    return tablero, barco
 
-def colocar_barcos(tablero,eslora=[4,3,3,2,2,2,1,1,1,1],):
+def colocar_barcos(tablero,eslora=[4,3,3,2,2,2,1,1,1,1]):
+    lista_barcos =[]
     for i in eslora:
-        tablero = colocar_barco(crear_barco_random(i,tablero), tablero)
+        tablero,barco = colocar_barco(crear_barco_random(i,tablero), tablero)
+        lista_barcos.append(barco)
+    return tablero, lista_barcos
+
+def verificar_hundidos(lista_barcos,tablero):
+    for eslora in lista_barcos:
+        l_barco = len(eslora)
+
+        for fila, columna in eslora:
+            if tablero[fila,columna] == "X":
+                l_barco -= 1
+
+        if l_barco == 0:
+            print("¡Barco hundido!")
+            for fila, columna in eslora:
+                tablero[fila,columna] = "■"
+
     return tablero
 
-def input1(tablero):
-    entrada=True
-    while entrada:
-        try:
-            fila=int(input("¿En qué fila vas a atacar? (Valores válidos: 0-"+str(len(tablero)-1)+")\n"))
-            if fila>=0 and fila<=len(tablero)-1:
-                entrada=False
-                return fila
-            else:
-                print("No es un valor válido, por favor vuelve a intentarlo","\n")
-                time.sleep(0.3)
+def disparar(casilla, tablero):
+    repetir = True
 
-        except:
-            print("No es un valor válido, por favor vuelve a intentarlo","\n")
-            time.sleep(0.3)
+    if tablero[casilla] == "_":
+        print("Agua")
+        tablero[casilla] = "A"
+        repetir = False
+
+    elif tablero[casilla] == "O":
+        print("Tocado, vuelves a disparar")
+        tablero[casilla] = "X"
+
+    else:
+        print("Ya has disparado aquí, vuelve a intentarlo")
+
+    return tablero, repetir
+
+def disparo_random(tablero):
+    fallo = True
+
+    while fallo:
+        fila_a = random.randint(0,len(tablero)-1)
+        columna_a = random.randint(0,len(tablero)-1)
+        casilla = (fila_a,columna_a)
+    
+        repetir = False
+    
+        if tablero[casilla] == "_":
+            print("Máquina dispara en:",casilla)
+            print("Agua")
+            tablero[casilla] = "A"
+            fallo = False
+
+        elif tablero[casilla] == "O":
+            print("Máquina dispara en:",casilla)
+            print("Tocado, la máquina dispara de nuevo")
+            tablero[casilla] = "X"
+            repetir = True
+            fallo = False
+
+        else:
             continue
-        
-def input2(tablero):
+
+    return tablero, repetir
+
+def evitar_errores_entrada(texto,variable_a_medir):
     entrada=True
     while entrada:
         try:
-            fila=int(input("¿En qué columna vas a atacar? (Valores válidos: 0-"+str(len(tablero)-1)+")\n"))
-            if fila>=0 and fila<=len(tablero)-1:
+            entrada_jugador = int(input(str(texto)+"(Valores válidos: 0-"+str(len(variable_a_medir)-1)+")\n"))
+            if entrada_jugador>=0 and entrada_jugador<=len(variable_a_medir)-1:
                 entrada=False
-                return fila
             else:
                 print("No es un valor válido, por favor vuelve a intentarlo","\n")
                 time.sleep(0.3)
-            
+                continue
         except:
             print("No es un valor válido, por favor vuelve a intentarlo","\n")
             time.sleep(0.3)
             continue
+
+    return entrada_jugador
+def input1(tablero):
+    fila = evitar_errores_entrada("Por favor, elige una fila",tablero)
+    columna = evitar_errores_entrada("Por favor, elige una columna",tablero)
+    return fila,columna
+
+def hundir_barco(lista_barcos,tablero):
+    print("Puedes hundir",len(lista_barcos),"barcos")
+
+    barco_hundido = evitar_errores_entrada("Elige un barco para hundir",lista_barcos)
+
+    for fila,columna in lista_barcos[barco_hundido]:
+
+        if tablero[fila,columna]=="■":
+            print("Ese barco ya está hundido, saliendo del modo hundir barco")
+            break
+        else:
+            tablero[fila,columna] = "■"
+
+    return tablero
+
+def finalizar(tablero):
+    tablero[tablero=="O"] = "■"
+    return tablero
+
+def sel_accion(tablero_1,tablero_2,lista_barcos_1,lista_barcos2):
+    accion=""
+    while accion=="" or (accion>0 and accion<5) :
+        accion = evitar_errores_entrada("Seleccione una acción:\n  0- Atacar.\n  1- Ver tablero.\n  2- Ver tablero rival (Sin niebla).\n  3- Hundir un barco enemigo.\n  4- Hundir barco propio.\n  5- Ganar la partida.\n","hundir")
+        match accion:
+            case 0:
+                repetir=True
+                return tablero_1,tablero_2,lista_barcos_1,lista_barcos2, repetir
+        
+            case 1:
+                print(tablero_1,"\n ------------------------------------------------------------\n")
+
+            case 2:
+                print(tablero_2,"\n ------------------------------------------------------------\n")
+
+            case 3:
+                hundir_barco(lista_barcos2,tablero_2)
+                ocultar_tablero(tablero_2)
+
+            case 4:
+                hundir_barco(lista_barcos_1, tablero_1)
+                print(tablero_1,"\n ------------------------------------------------------------\n")
+
+            case 5:
+                finalizar(tablero_2)
+                repetir=False
+                return tablero_1,tablero_2,lista_barcos_1,lista_barcos2, repetir
+                
+
+
--- main.py
+++ main.py
@@ -2,25 +2,42 @@ import funciones as f
 import time
 
 tablero_jugador = f.crear_tablero()
-tablero_jugador = f.colocar_barcos(tablero_jugador)
+tablero_jugador, barcos_jugador = f.colocar_barcos(tablero_jugador)
 
 tablero_maquina = f.crear_tablero()
-tablero_maquina = f.colocar_barcos(tablero_maquina)
+tablero_maquina, barcos_maquina  = f.colocar_barcos(tablero_maquina)
+bucle = True
 
 while "O" in tablero_jugador and "O" in tablero_maquina:
 
-    repetir=True
+    tablero_jugador, tablero_maquina, barcos_jugador, barcos_maquina, repetir = f.sel_accion(tablero_jugador, tablero_maquina, barcos_jugador, barcos_maquina)
+    
     while repetir:
-        tablero_maquina,repetir = f.disparar((f.input1(tablero_maquina),f.input2(tablero_maquina)), tablero_maquina)
-        print("\n",tablero_maquina,"\n")
-        print("------------------------------------------------------------\n")
+        tablero_maquina,repetir = f.disparar(f.input1(tablero_maquina), tablero_maquina)
+        tablero_maquina=f.verificar_hundidos(barcos_maquina,tablero_maquina)
+        f.ocultar_tablero(tablero_maquina)
         time.sleep(0.3)
+        if "O" not in tablero_maquina:
+            break
+        
+    if "O" not in tablero_maquina:
+        break
 
     repetir=True
     while repetir:
         tablero_jugador, repetir = f.disparo_random(tablero_jugador)
-        print("\n",tablero_jugador,"\n")
+        tablero_jugador=f.verificar_hundidos(barcos_jugador,tablero_jugador)
         print("------------------------------------------------------------\n")
         time.sleep(0.3)
+
+if "O" not in tablero_jugador:
+    print("\n \n Has perdido")
+    print("FIN DEL JUEGO")
+
+elif "O" not in tablero_maquina:
+    print("\n \n Has ganado")
+    print("FIN DEL JUEGO")
+else:
+    print("FIN DEL JUEGO")
 
 
