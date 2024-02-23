67bc70afc45673630d8e7b2e63b4c095a6b73364
JavierCalderon94 <javiercalderongarcia@gmail.com>
2024-02-23 09:06:05 +0100

    Version 1 - Operativa

--- funciones.py
+++ funciones.py
@@ -1,5 +1,6 @@
 import numpy as np
 import random
+import time
 
 def crear_tablero(tamaño=(10,10)):
     return np.full(tamaño, "_")
@@ -15,22 +16,54 @@ def colocar_barcos(barcos, tablero):
     return tablero
 
 def disparar(casilla, tablero):
+    repetir = True
+
     if tablero[casilla] == "_":
         print("Agua")
         tablero[casilla] = "A"
+        repetir = False
+
     elif tablero[casilla] == "O":
-        print("Tocado")
+        print("Tocado, vuelves a disparar")
         tablero[casilla] = "X"
+
     else:
-        print("Ya has disparado aquí")
-    return tablero
+        print("Ya has disparado aquí, vuelve a intentarlo")
+
+    return tablero, repetir
+
+def disparo_random(tablero):
+    fila_a = random.randint(0,len(tablero)-1)
+    columna_a = random.randint(0,len(tablero)-1)
+    casilla = (fila_a,columna_a)
+    fallo = True
+    repetir = False
+
+    while fallo:
+        if tablero[casilla] == "_":
+            print("Agua")
+            tablero[casilla] = "A"
+            fallo = False
+
+        elif tablero[casilla] == "O":
+            print("Tocado, la máquina dispara de nuevo")
+            tablero[casilla] = "X"
+            repetir = True
+            fallo = False
+
+        else:
+            continue
+
+    return tablero, repetir
 
 def crear_barco_random(eslora,tablero):
     barco =[]
     while len(barco) < eslora:
-        fila_a = random.randint(0,9)
-        columna_a = random.randint(0,9)
+        fila_a = random.randint(0,len(tablero)-1)
+        columna_a = random.randint(0,len(tablero)-1)
 
+        if tablero[fila_a,columna_a]=="O":
+                continue
         orientacion = random.choice(["S","O","E","N"])
 
         barco = [(fila_a, columna_a)]
@@ -56,3 +89,47 @@ def crear_barco_random(eslora,tablero):
                 barco.append((fila_a, columna_a))
 
     return barco
+
+def colocar_barco(barco, tablero):
+    for casilla in barco:
+        tablero[casilla] = "O"
+    return tablero
+
+def colocar_barcos(tablero,eslora=[4,3,3,2,2,2,1,1,1,1],):
+    for i in eslora:
+        tablero = colocar_barco(crear_barco_random(i,tablero), tablero)
+    return tablero
+
+def input1(tablero):
+    entrada=True
+    while entrada:
+        try:
+            fila=int(input("¿En qué fila vas a atacar? (Valores válidos: 0-"+str(len(tablero)-1)+")\n"))
+            if fila>=0 and fila<=len(tablero)-1:
+                entrada=False
+                return fila
+            else:
+                print("No es un valor válido, por favor vuelve a intentarlo","\n")
+                time.sleep(0.3)
+
+        except:
+            print("No es un valor válido, por favor vuelve a intentarlo","\n")
+            time.sleep(0.3)
+            continue
+        
+def input2(tablero):
+    entrada=True
+    while entrada:
+        try:
+            fila=int(input("¿En qué columna vas a atacar? (Valores válidos: 0-"+str(len(tablero)-1)+")\n"))
+            if fila>=0 and fila<=len(tablero)-1:
+                entrada=False
+                return fila
+            else:
+                print("No es un valor válido, por favor vuelve a intentarlo","\n")
+                time.sleep(0.3)
+            
+        except:
+            print("No es un valor válido, por favor vuelve a intentarlo","\n")
+            time.sleep(0.3)
+            continue
--- main.py
+++ main.py
@@ -1,2 +1,26 @@
-print("hola mundo   ")
-print("Hola otra vez")
+import funciones as f
+import time
+
+tablero_jugador = f.crear_tablero()
+tablero_jugador = f.colocar_barcos(tablero_jugador)
+
+tablero_maquina = f.crear_tablero()
+tablero_maquina = f.colocar_barcos(tablero_maquina)
+
+while "O" in tablero_jugador and "O" in tablero_maquina:
+
+    repetir=True
+    while repetir:
+        tablero_maquina,repetir = f.disparar((f.input1(tablero_maquina),f.input2(tablero_maquina)), tablero_maquina)
+        print("\n",tablero_maquina,"\n")
+        print("------------------------------------------------------------\n")
+        time.sleep(0.3)
+
+    repetir=True
+    while repetir:
+        tablero_jugador, repetir = f.disparo_random(tablero_jugador)
+        print("\n",tablero_jugador,"\n")
+        print("------------------------------------------------------------\n")
+        time.sleep(0.3)
+
+
